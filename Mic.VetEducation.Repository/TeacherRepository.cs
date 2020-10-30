701b8b89a18c315a6b804acd28e1e6754442c691
avagyansamvel07@gmail.com <avagyansamvel07@gmail.com>
2020-10-30 09:02:12 -0700

    TeacherRepository inherit from BaseRepository

--- Mic.VetEducation.Repository/TeacherRepository.cs
+++ Mic.VetEducation.Repository/TeacherRepository.cs
@@ -1,65 +1,14 @@
 ﻿using Mic.VetEducation.Repository.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Mic.VetEducation.Repository
 {
-    public class TeacherRepository
+    public class TeacherRepository : BaseRepository<Teacher>
     {
-        bool _isLoaded;
-        Dictionary<int, Teacher> _source;
-
         public TeacherRepository() : this("Teachers.xml")
-        {
-
-        }
+        { }
 
         public TeacherRepository(string fileName)
-        {
-            FileName = fileName;
-            _source = new Dictionary<int, Teacher>();
-        }
-
-        public string FileName { get; private set; }
-
-        public IEnumerable<Teacher> ReadAll()
-        {
-            if (!_isLoaded)
-            {
-                _isLoaded = true;
-
-                try
-                {
-                    _source = XmlHelper
-                        .ReadFromXml<Teacher>(FileName)
-                        .ToDictionary(t => t.Id);
-                }
-                catch (Exception ex)
-                {
-
-                }
-            }
-
-            return _source.Values;
-        }
-
-        public List<Teacher> ReadToList()
-        {
-            return ReadAll().ToList();
-        }
-
-        public void Add(Teacher t)
-        {
-            _source.Add(t.Id, t);
-        }
-
-        public void SaveChanges()
-        {
-            XmlHelper.SaveToXml<Teacher>(_source.Values.ToList(), FileName);
-        }
-
-
+            : base(fileName)
+        { }
     }
 }
