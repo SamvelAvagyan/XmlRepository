20635624fa82a78a36bfdc88e90f884640601847
avagyansamvel07@gmail.com <avagyansamvel07@gmail.com>
2020-10-30 09:06:48 -0700

    User and Student Repositories inherit from BaseRepository

--- Mic.VetEducation.Repository/StudentRepository.cs
+++ Mic.VetEducation.Repository/StudentRepository.cs
@@ -1,63 +1,13 @@
 ﻿using Mic.VetEducation.Repository.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Mic.VetEducation.Repository
 {
-    public class StudentRepository
+    public class StudentRepository : BaseRepository<Student>
     {
-        bool _isLoaded;
-        Dictionary<int, Student> _source;
-
         public StudentRepository() : this("Students.xml") { }
 
         public StudentRepository(string fileName)
-        {
-            FileName = fileName;
-            _source = new Dictionary<int, Student>();
-        }
-
-        public string FileName { get; private set; }
-
-        public IEnumerable<Student> ReadAll()
-        {
-            if (!_isLoaded)
-            {
-                _isLoaded = true;
-                try
-                {
-                    _source = XmlHelper
-                        .ReadFromXml<Student>(FileName)
-                        .ToDictionary(st => st.Id);
-                }
-                catch(Exception ex)
-                {
-                    
-                }
-            }
-
-            return _source.Values;
-        }
-
-        public void Add(Student st)
-        {
-            _source.Add(st.Id, st);
-        }
-
-        public bool Remove(int id)
-        {
-            return _source.Remove(id);
-        }
-
-        public List<Student> ReadToList()
-        {
-            return ReadAll().ToList();
-        }
-
-        public void SaveChanges()
-        {
-            XmlHelper.SaveToXml(_source.Values.ToList(), FileName);
-        }
+            : base(fileName)
+        { }   
     }
 }
--- Mic.VetEducation.Repository/UserRepository.cs
+++ Mic.VetEducation.Repository/UserRepository.cs
@@ -1,63 +1,14 @@
 ﻿using Mic.VetEducation.Repository.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Mic.VetEducation.Repository
 {
-    public class UserRepository
+    public class UserRepository : BaseRepository<User>
     {
-        bool _isLoaded;
-        Dictionary<int, User> _source;
-
         public UserRepository() : this("Users.xml")
-        {
-
-        }
+        { }
 
         public UserRepository(string fileName)
-        {
-            FileName = fileName;
-            _source = new Dictionary<int, User>();
-        }
-
-        public string FileName { get; private set; }
-
-        public IEnumerable<User> ReadAll()
-        {
-            if (!_isLoaded)
-            {
-                _isLoaded = true;
-
-                try
-                {
-                    _source = XmlHelper
-                        .ReadFromXml<User>(FileName)
-                        .ToDictionary(u => u.Id);
-                }
-                catch (Exception ex)
-                {
-
-                }
-            }
-
-            return _source.Values;
-        }
-
-        public List<User> ReadToList()
-        {
-            return ReadAll().ToList();
-        }
-
-        public void Add(User u)
-        {
-            _source.Add(u.Id, u);
-        }
-
-        public void SaveChanges()
-        {
-            XmlHelper.SaveToXml<User>(_source.Values.ToList(), FileName);
-        }
+            : base(fileName)
+        { }      
     }
 }
