48a313cf5bb8853f026f22416bc329df9a7481e7
avagyansamvel07@gmail.com <avagyansamvel07@gmail.com>
2020-10-30 09:10:31 -0700

    Add UniversityRepository

---
+++ Mic.VetEducation.Repository/Models/University.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mic.VetEducation.Repository.Models
+{
+    public class University : BaseModel
+    {
+    }
+}
---
+++ Mic.VetEducation.Repository/UniversityRepository.cs
@@ -0,0 +1,13 @@
+﻿using Mic.VetEducation.Repository.Models;
+
+namespace Mic.VetEducation.Repository
+{
+    public class UniversityRepository : BaseRepository<University>
+    {
+        public UniversityRepository() : this("University.xml") { }
+
+        public UniversityRepository(string fileName)
+            : base(fileName)
+        { }
+    }
+}
