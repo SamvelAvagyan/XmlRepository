6559d811d97cb615a1c1c3c7d83aebb218ac243c
SamvelAvagyan <67542833+SamvelAvagyan@users.noreply.github.com>
2020-11-02 08:13:30 -0800

    Split XmlRepository impl to separate dll (#2)

---
+++ Mic.VetEducation.Repository.XmlRepositories/Migrations/StudentMigration.cs
@@ -0,0 +1,23 @@
+﻿using Mic.VetEducation.Repository.XmlRepositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mic.VetEducation.Repository.XmlRepositories.Migrations
+{
+    public static class StudentMigration
+    {
+        public static void AddInStudent()
+        {
+            //var repo = new StudentRepository();
+            //var source = repo.ReadToList();
+            //var rnd = new Random();
+            //for (int i = 0; i < source.Count; i++)
+            //{
+            //    source[i].Id = i + 1;
+            //    source[i].Mark = (byte)rnd.Next(1, 21);
+            //}
+            //repo.SaveChanges();
+        }
+    }
+}
